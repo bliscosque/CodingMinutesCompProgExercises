3a4c3ccbbc28ee98589054847caf673d9a66ab9f
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-01-21 21:58:08 -0600

    Add project files.

---
+++ CodingMinutesExercises/CodingMinutesExercises.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int firstUniqChar(string s) {
+    map<char, int> m;
+    for (char ch : s) {
+        if (m.count(ch) == 0) 
+            m[ch] = 1;
+        else m[ch] = m[ch] + 1; //apenas esta linha dentro do for ja bastaria, uma vez que int inicia com 0 neste caso
+    }
+    for (int i = 0; i < s.size(); i++) {
+        if (m[s[i]] == 1) return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    cout << firstUniqChar("codingminutes") << endl;
+    cout << firstUniqChar("aabb") << endl;
+}
+
